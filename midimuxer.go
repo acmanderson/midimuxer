b9f97bb441b5d702b83ee9ca73add0e0b28f36b6
Chase Anderson <acmanderson@gmail.com>
2018-06-20 16:51:51 -0500

    use config functions to define filters and transformers on routes

--- midimuxer.go
+++ midimuxer.go
@@ -40,8 +40,8 @@ func NewRouter(sources ...Source) *Router {
 	}
 }
 
-func (m *Router) Start() error {
-	for _, source := range m.sources {
+func (r *Router) Start() error {
+	for _, source := range r.sources {
 		if err := source.Start(); err != nil {
 			return err
 		}
@@ -49,8 +49,8 @@ func (m *Router) Start() error {
 	return nil
 }
 
-func (m *Router) Stop() error {
-	for _, source := range m.sources {
+func (r *Router) Stop() error {
+	for _, source := range r.sources {
 		if err := source.Stop(); err != nil {
 			return err
 		}
@@ -58,9 +58,9 @@ func (m *Router) Stop() error {
 	return nil
 }
 
-func (m *Router) Inputs() []Device {
+func (r *Router) Inputs() []Device {
 	var inputs []Device
-	for _, source := range m.sources {
+	for _, source := range r.sources {
 		for _, device := range source.Devices() {
 			if device.IsInput() {
 				inputs = append(inputs, device)
@@ -70,9 +70,9 @@ func (m *Router) Inputs() []Device {
 	return inputs
 }
 
-func (m *Router) Outputs() []Device {
+func (r *Router) Outputs() []Device {
 	var outputs []Device
-	for _, source := range m.sources {
+	for _, source := range r.sources {
 		for _, device := range source.Devices() {
 			if device.IsOutput() {
 				outputs = append(outputs, device)
@@ -82,34 +82,54 @@ func (m *Router) Outputs() []Device {
 	return outputs
 }
 
-func (m *Router) AddRoute(input Device, output Device, filters []Filter, transformers []Transformer) error {
-	_, ok := m.routes[input]
-	m.routes[input] = append(m.routes[input], &route{output, filters, transformers})
+func WithFilter(filter Filter) func(*route) {
+	return func(r *route) {
+		r.filters = append(r.filters, filter)
+	}
+}
+
+func WithTransformer(transformer Transformer) func(*route) {
+	return func(r *route) {
+		r.transformers = append(r.transformers, transformer)
+	}
+}
+
+func (r *Router) routeEvents(input Device, events <-chan *Event) {
+	for event := range events {
+		for _, route := range r.routes[input] {
+			sendEvent := true
+			for _, filter := range route.filters {
+				if !filter.Filter(*event) {
+					sendEvent = false
+					break
+				}
+			}
+			if sendEvent {
+				for _, transformer := range route.transformers {
+					*event = transformer.Transform(*event)
+				}
+				outgoing, _ := route.device.Outgoing()
+				outgoing <- &Event{event.Status, event.Data1, event.Data2}
+			}
+		}
+	}
+}
+
+func (r *Router) AddRoute(input Device, output Device, routeConfigs ...func(*route)) error {
+	route := &route{device: output}
+	for _, config := range routeConfigs {
+		config(route)
+	}
+
+	_, ok := r.routes[input]
+	r.routes[input] = append(r.routes[input], route)
 
 	if !ok {
 		incoming, err := input.Incoming()
 		if err != nil {
 			return err
 		}
-		go func(device Device, events <-chan *Event) {
-			for event := range events {
-				for _, route := range m.routes[device] {
-					sendEvent := true
-					for _, filter := range route.filters {
-						if !filter.Filter(*event) {
-							sendEvent = false
-						}
-					}
-					if sendEvent {
-						for _, transformer := range route.transformers {
-							*event = transformer.Transform(*event)
-						}
-						outgoing, _ := route.device.Outgoing()
-						outgoing <- &Event{event.Status, event.Data1, event.Data2}
-					}
-				}
-			}
-		}(input, incoming)
+		go r.routeEvents(input, incoming)
 	}
 
 	return nil
